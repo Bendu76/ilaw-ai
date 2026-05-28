a97ceace13bdba50fd03239bab419f209e82fefb
Luvismindo <dluvismindojr@gmail.com>
2026-05-28 20:22:45 +0800

    fix auth routes

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 app.use(express.static("public"));
 
 app.use("/auth", authRoutes);
