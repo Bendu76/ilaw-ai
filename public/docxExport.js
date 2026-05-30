bce731f3c801905546821a221da59ce11bc42959
Luvismindo <dluvismindojr@gmail.com>
2026-05-30 19:30:54 +0800

    Fix DOCX export script

--- public/docxExport.js
+++ public/docxExport.js
@@ -1,33 +1,31 @@
-const isPaid =
-  localStorage.getItem(
-    "isPaid"
-  );
-
-if (isPaid !== "true") {
-
-  alert(
-    "Please purchase credits first."
-  );
-
-  window.location.href =
-    "payment.html";
-
-  return;
-
-}
-
 function downloadDOCX() {
 
-  const content =
-    document.getElementById("output").innerText;
+  const isPaid =
+    localStorage.getItem("isPaid");
 
-  const blob = new Blob(
-    [content],
-    {
-      type:
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    }
-  );
+  if (isPaid !== "true") {
+
+    alert(
+      "Please purchase credits first."
+    );
+
+    window.location.href =
+      "payment.html";
+
+    return;
+  }
+
+  const content =
+    document.getElementById("output").innerHTML;
+
+  const blob =
+    new Blob(
+      [content],
+      {
+        type:
+          "application/msword"
+      }
+    );
 
   const link =
     document.createElement("a");
@@ -36,12 +34,10 @@ function downloadDOCX() {
     URL.createObjectURL(blob);
 
   link.download =
-    "ILAW_Lesson_Plan.docx";
-
-  document.body.appendChild(link);
+    "ILAW_Lesson_Plan.doc";
 
   link.click();
-
-  document.body.removeChild(link);
-
 }
+
+window.downloadDOCX =
+  downloadDOCX;
